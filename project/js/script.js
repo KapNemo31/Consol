309eb74d36d42caf2abe3f041d200cb2efb92936
DarkKing <93931054+KapNemo31@users.noreply.github.com>
2022-03-29 12:02:16 +0300

    Add files via upload

---
+++ project/js/script.js
@@ -0,0 +1,40 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+ww
+5) Добавить нумерацию выведенных фильмов */
+
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+
+const img = document.querySelectorAll('img');
+
+img.forEach(item => {
+    item.remove();
+});
+const com = document.querySelector('.promo__genre');
+com.innerHTML = "<div class='promo__genre'>ДРАММА</div>";
+
+//
+
+const bg = document.querySelector('.promo__bg');
+console.log(bg);
+bg.style.cssText = "background-img: url('../img/bg.jpg') ;"
